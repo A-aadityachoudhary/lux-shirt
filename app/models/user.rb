04a422042ba65ab31c4001adaa8192ed533de25e
Aaditya Choudhary <aadityach812@gmail.com>
2026-05-30 17:50:01 +0530

    adding controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-    has_many :products
-    has_one :orders
+  has_secure_password
+  has_many :products
+  has_many :orders
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
+  resources :products
+  resources :orders
+  resources :order_items
+  namespace :admin do
+    resources :products
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
