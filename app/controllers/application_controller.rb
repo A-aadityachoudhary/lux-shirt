795accda5b36c328eed174062f085ec27411c583
Aaditya Choudhary <aadityach812@gmail.com>
2026-06-05 01:06:15 +0530

    fix: remove rate_limit to fix solid_cache 500, guard cart for guests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,20 +18,22 @@ class ApplicationController < ActionController::Base
   end
 
   def current_cart
-    if session[:cart_id]
-      # Look up existing cart
-      cart = Cart.find_by(id: session[:cart_id])
-      if cart.present?
-        @current_cart = cart
-      else
-        session[:cart_id] = nil
-      end
-    end
-    if session[:cart_id].nil?
-      @current_cart = Cart.create
-      session[:cart_id] = @current_cart.id
+  return unless Current.user  # Don't create cart for guests
+
+  if session[:cart_id]
+    cart = Cart.find_by(id: session[:cart_id])
+    if cart.present?
+      @current_cart = cart
+    else
+      session[:cart_id] = nil
     end
   end
+
+  if session[:cart_id].nil?
+    @current_cart = Cart.create
+    session[:cart_id] = @current_cart.id
+  end
+end
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
