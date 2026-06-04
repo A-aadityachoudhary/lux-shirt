43704eee8d12a35a3b564f5e15a8b857f551fc19
Aaditya Choudhary <aadityach812@gmail.com>
2026-06-05 00:19:07 +0530

    Change db:migrate to db:prepare to fix missing solid cache table

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,6 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate
+
+# CRITICAL: This checks standard migrations AND builds the missing Solid Cache tables
+bundle exec rails db:prepare
